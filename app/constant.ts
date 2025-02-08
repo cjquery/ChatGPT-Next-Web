4d7820556cbab43c5d60da5dab66ce528d3e85bc
caojiaqi <max.cao@hstong.com>
2025-02-08 14:28:13 +0800

    自定义模型调整

--- app/constant.ts
+++ app/constant.ts
@@ -755,7 +755,6 @@ export const DEFAULT_MODELS = [
       sorted: 13,
     },
   })),
-    /*
   ...siliconflowModels.map((name) => ({
     name,
     available: true,
@@ -766,7 +765,7 @@ export const DEFAULT_MODELS = [
       providerType: "siliconflow",
       sorted: 14,
     },
-  })),*/
+  })),
 ] as const;
 
 export const CHAT_PAGE_SIZE = 15;
