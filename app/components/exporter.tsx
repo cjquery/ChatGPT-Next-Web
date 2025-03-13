746faa2f71ce3a510e6c1e2c5edc02e915f8999a
caojiaqi <max.cao@hstong.com>
2025-03-13 16:54:32 +0800

    自定义模型调整

--- app/components/exporter.tsx
+++ app/components/exporter.tsx
@@ -526,7 +526,7 @@ export function ImagePreviewer(props: {
           <div>
             <div className={styles["main-title"]}>NextChat</div>
             <div className={styles["sub-title"]}>
-              github.com/cjquery/ChatGPT-Next-Web
+              github.com/ChatGPTNextWeb/ChatGPT-Next-Web
             </div>
             <div className={styles["icons"]}>
               <MaskAvatar avatar={config.avatar} />
